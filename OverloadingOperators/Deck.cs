65763f4ea63ceaac27ee7e1c839d7107bc128f06
Zack Hendrix <Zackhendrix01@gmail.com>
2022-02-21 21:16:16 -0600

    Initial commit

---
+++ OverloadingOperators/Deck.cs
@@ -0,0 +1,28 @@
+﻿namespace OverloadingOperators
+{
+    public class Deck
+    {
+        public string Type { get; set; }
+        public int NumberOfCards { get; set; }
+
+        public Deck(string type, int numberOfCards)
+        {
+            Type = type;
+            NumberOfCards = numberOfCards;
+        }
+
+        //Overloading the + operator to add 2 deck objects together
+        public static Deck operator +(Deck deck1, Deck deck2)
+        {
+            string types = $"{deck1.Type}, {deck2.Type}";
+            int newNumberOfCards = deck1.NumberOfCards + deck2.NumberOfCards;
+            return new Deck(types, newNumberOfCards);
+        }
+
+        public override string ToString()
+        {
+            return $"Deck type: {Type} \n" +
+                   $"Number of cards: {NumberOfCards} \n\n";
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# Learning_Sandbox
+ Series of .NET Core console applications. Place to practice some foreign concepts and try new things,
