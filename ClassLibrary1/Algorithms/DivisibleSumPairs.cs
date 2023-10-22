128a771cf49b1b709e8f8b04e735d8abf6f8336b
Zack Hendrix <Zackhendrix01@gmail.com>
2023-10-21 20:50:25 -0500

    Added new Divisible sum pairs class to algorithms Added additional tests

---
+++ ClassLibrary1/Algorithms/DivisibleSumPairs.cs
@@ -0,0 +1,24 @@
+﻿namespace Learning.Algorithms
+{
+    public class DivisibleSumPairs
+    {
+        //Given a list of integers and a positive integer (divisor), determine the number of (i,j) pairs where i < j and ar[i] + ar[j] is divisible by the divisor. 
+        public int GetTotalDivisableSumPairs(int divisor, List<int> ar)
+        { 
+            int totalDivisable = 0;
+
+            for (int i = 0; i < ar.Count; i++)
+            {
+                for (int j = 0; j < ar.Count; j++)
+                {
+                    if (i < j && (ar[i] + ar[j]) % divisor == 0)
+                    {
+                        totalDivisable++;
+                    }
+                }
+            }
+
+            return totalDivisable;
+        }
+    }
+}
---
+++ Learning_Sandbox.Tests/AlgorithmTests/DivisibleSumPairTests.cs
@@ -0,0 +1,59 @@
+﻿using Learning.Algorithms;
+
+namespace Learning_Sandbox.Tests.AlgorithmTests
+{
+
+    public class DivisibleSumPairTests
+    {
+        private readonly DivisibleSumPairs _divisibleSumPairs = new();
+
+        [Fact]
+        public void GetTotalDivisableSumPairs_Returns_5()
+        {
+            //arrange
+            int divisor = 3;
+            List<int> ar = new() { 1, 3, 2, 6, 1, 2 };
+
+            int expectedSumPairs = 5;
+
+            //act
+            int actualSumPairs = _divisibleSumPairs.GetTotalDivisableSumPairs(divisor, ar);
+
+            //assert
+            Assert.Equal(expectedSumPairs, actualSumPairs);
+        }
+
+        [Fact]
+        public void GetTotalDivisableSumPairs_Returns_3()
+        {
+            //arrange
+            int divisor = 3;
+            List<int> ar = new() { 2, 8, 6, 8, 4 };
+
+            int expectedSumPairs = 3;
+
+            //act
+            int actualSumPairs = _divisibleSumPairs.GetTotalDivisableSumPairs(divisor, ar);
+
+            //assert
+            Assert.Equal(expectedSumPairs, actualSumPairs);
+        }
+
+        [Fact]
+        public void GetTotalDivisableSumPairs_Returns_4()
+        {
+            //arrange
+            int divisor = 2;
+            List<int> ar = new() { 5, 9, 10, 7, 4 };
+
+            int expectedSumPairs = 4;
+
+            //act
+            int actualSumPairs = _divisibleSumPairs.GetTotalDivisableSumPairs(divisor, ar);
+
+            //assert
+            Assert.Equal(expectedSumPairs, actualSumPairs);
+        }
+
+    }
+}
