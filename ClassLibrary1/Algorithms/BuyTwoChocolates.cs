4b94e61ac60755572a00b39c7e45d36aacf6e4dd
Zack Hendrix <Zackhendrix01@gmail.com>
2024-01-05 21:05:22 -0600

    Added new solution for LeetCode problem

---
+++ ClassLibrary1/Algorithms/BuyTwoChocolates.cs
@@ -0,0 +1,31 @@
+﻿namespace Learning.Algorithms
+{
+    public class BuyTwoChocolates
+    {
+
+        //Explanation: https://leetcode.com/problems/buy-two-chocolates/
+        public int BuyChoco(int[] prices, int money)
+        {
+            int numberOfChocolates = 0;
+            int moneyTemp = money;
+
+            Array.Sort(prices);
+
+            for (int i = 0; i < prices.Length; i++)
+            {
+                if (moneyTemp > 0 && moneyTemp >= prices[i])
+                {
+                    moneyTemp -= prices[i];
+                    numberOfChocolates++;
+                }
+
+                if (numberOfChocolates >= 2)
+                {
+                    return moneyTemp;
+                }
+            }
+
+            return money;
+        }
+    }
+}
---
+++ Learning_Sandbox.Tests/AlgorithmTests/BuyTwoChocolateTests.cs
@@ -0,0 +1,45 @@
+﻿using ClassLibrary1.Algorithms;
+using Learning.Algorithms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning_Sandbox.Tests.AlgorithmTests
+{
+    public class BuyTwoChocolateTests
+    {
+        private readonly BuyTwoChocolates _buyTwoChocolates = new();
+
+        [Fact]
+        public void BuyTwoChocolates_Returns_3()
+        {
+            //arrange
+            int[] prices = new int[3] { 3,2,3 };
+            int money = 3;
+            int expectedResult = 3;
+
+            //act
+            int actualResult = _buyTwoChocolates.BuyChoco(prices, money);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void BuyTwoChocolates_Returns_0()
+        {
+            //arrange
+            int[] prices = new int[3] { 1, 2, 2 };
+            int money = 3;
+            int expectedResult = 0;
+
+            //act
+            int actualResult = _buyTwoChocolates.BuyChoco(prices, money);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
