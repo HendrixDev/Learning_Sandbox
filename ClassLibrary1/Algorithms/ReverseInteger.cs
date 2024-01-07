4c515d9ef805626f5188ea68911c6dc08c984eac
Zack Hendrix <Zackhendrix01@gmail.com>
2024-01-06 21:05:46 -0600

    Added ReverseInteger algorithm and tests

---
+++ ClassLibrary1/Algorithms/ReverseInteger.cs
@@ -0,0 +1,27 @@
+﻿namespace Learning.Algorithms
+{
+    public class ReverseInteger
+    {
+        //Explanation: https://leetcode.com/problems/reverse-integer/
+        public int Reverse(int x)
+        {
+            return int.TryParse(ReverseString(x.ToString()), out int reversed) ? reversed : 0;
+        }
+
+        string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            Array.Reverse(charArray);
+
+            if (charArray.Contains('-'))
+            {
+                List<char> list = charArray.ToList();
+                list.Remove('-');
+                list.Insert(0, '-');
+                return new string(list.ToArray());
+            }
+
+            return new string(charArray);
+        }
+    }
+}
---
+++ Learning_Sandbox.Tests/AlgorithmTests/ReverseIntegerTests.cs
@@ -0,0 +1,93 @@
+﻿using Learning.Algorithms;
+
+namespace Learning_Sandbox.Tests.AlgorithmTests
+{
+    public class ReverseIntegerTests
+    {
+        private readonly ReverseInteger _reverseInteger = new();
+
+        [Fact]
+        public void ReverseInteger_Returns_654321()
+        {
+            //arrange
+            int intToReverse = 123456;
+            int expectedResult = 654321;
+
+            //act
+            int actualResult = _reverseInteger.Reverse(intToReverse);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ReverseInteger_Returns_96369()
+        {
+            //arrange
+            int intToReverse = 96369;
+            int expectedResult = 96369;
+
+            //act
+            int actualResult = _reverseInteger.Reverse(intToReverse);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ReverseInteger_Returns_Negative_321()
+        {
+            //arrange
+            int intToReverse = -123;
+            int expectedResult = -321;
+
+            //act
+            int actualResult = _reverseInteger.Reverse(intToReverse);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ReverseInteger_Returns_Negative_999()
+        {
+            //arrange
+            int intToReverse = -999;
+            int expectedResult = -999;
+
+            //act
+            int actualResult = _reverseInteger.Reverse(intToReverse);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ReverseInteger_Returns_21()
+        {
+            //arrange
+            int intToReverse = 120;
+            int expectedResult = 21;
+
+            //act
+            int actualResult = _reverseInteger.Reverse(intToReverse);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void ReverseInteger_Returns_55()
+        {
+            //arrange
+            int intToReverse = 550;
+            int expectedResult = 55;
+
+            //act
+            int actualResult = _reverseInteger.Reverse(intToReverse);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+    }
+}
