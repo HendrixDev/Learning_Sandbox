ad39c2eb49194b4684d261783b0a7673741609a6
Zack Hendrix <Zackhendrix01@gmail.com>
2024-05-05 20:55:37 -0500

    Finished adding tests for MedianOfTwoSortedArray algorithm

--- Learning_Sandbox.Tests/AlgorithmTests/BuyTwoChocolateTests.cs
+++ Learning_Sandbox.Tests/AlgorithmTests/BuyTwoChocolateTests.cs
@@ -1,10 +1,4 @@
-﻿using ClassLibrary1.Algorithms;
-using Learning.Algorithms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Learning.Algorithms;
 
 namespace Learning_Sandbox.Tests.AlgorithmTests
 {
--- Learning_Sandbox.Tests/AlgorithmTests/MedianOfTwoSortedArraysTests.cs
+++ Learning_Sandbox.Tests/AlgorithmTests/MedianOfTwoSortedArraysTests.cs
@@ -1,9 +1,4 @@
 ﻿using Learning.Algorithms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Learning_Sandbox.Tests.AlgorithmTests
 {
@@ -11,16 +6,16 @@ namespace Learning_Sandbox.Tests.AlgorithmTests
     {
         private readonly MedianOfTwoSortedArrays _medianOfTwoSortedArrays = new();
         private int[] _array1 = new int[] { 5, 1, 3, 4, 2 };
-        private int[] _array2 = new int[] { 9, 7, 8, 10, 6 };
+        private int[] _array2 = new int[] { 9, 7, 8, 10, 6, 11 };
 
         [Fact]
-        public void MergeArray_ReturnsLength_10()
+        public void MergeArray_ReturnsLength_11()
         {
             //arrange
             int[] firstArray = _array1;
             int[] secondArray = _array2;
             int[] mergedArray;
-            int expectedLength = 10;
+            int expectedLength = 11;
 
             //act
             mergedArray = _medianOfTwoSortedArrays.MergeArrays(firstArray, secondArray);
@@ -51,5 +46,52 @@ namespace Learning_Sandbox.Tests.AlgorithmTests
             Assert.Equal(expectedValueAtIndexNine, mergedArray[9]);
         }
 
+        [Fact]
+        public void SortArray_ActuallySorts()
+        {
+            //arrange
+            int[] arrayToSort = _medianOfTwoSortedArrays.MergeArrays(_array1, _array2);
+
+            //act
+            _medianOfTwoSortedArrays.SortArray(arrayToSort);
+
+            //assert
+            Assert.Equal(1, arrayToSort[0]);
+            Assert.Equal(3, arrayToSort[2]);
+            Assert.Equal(5, arrayToSort[4]);
+            Assert.Equal(7, arrayToSort[6]);
+            Assert.Equal(9, arrayToSort[8]);
+            Assert.Equal(11, arrayToSort[10]);
+        }
+
+        [Fact]
+        public void CalculateMedian_OddNumberOfElements_Returns_3()
+        {
+            //arrange
+            int[] array = _array1;
+            double expectedResult = 3;
+            _medianOfTwoSortedArrays.SortArray(array);
+
+            //act
+            double actualResult = _medianOfTwoSortedArrays.CalculateMedianOfSortedArray(array);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void CalculateMedian_EvenNumberOfElements_Returns_8_5()
+        {
+            //arrange
+            int[] array = _array2;
+            double expectedResult = 8.5;
+            _medianOfTwoSortedArrays.SortArray(array);
+
+            //act
+            double actualResult = _medianOfTwoSortedArrays.CalculateMedianOfSortedArray(array);
+
+            //assert
+            Assert.Equal(expectedResult, actualResult);
+        }
     }
 }
